eaa2eaed77550eb8513dbe5bffa14e0cf6b278a1
kaedeng <156162245+kaedeng@users.noreply.github.com>
2026-02-26 16:19:06 -0500

    change analytics export to next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import { Providers } from '@/providers/theme'
 import './globals.css'
 
